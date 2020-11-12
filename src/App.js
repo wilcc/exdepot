f404cd2ee7c3ac06e7796a77143ba302dd4698d8
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-11-12 14:36:25 -0500

    created ./dashboard/dashboard.scss

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I'm a custom Message this is the beginning of a new project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
