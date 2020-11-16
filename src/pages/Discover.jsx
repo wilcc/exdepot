b61d2f0848874d1f6484569fe9efc2ab8a622adb
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-11-16 13:09:17 -0500

    worked out some with Popular, Ending Soon, Suggested for you and improved CSS for the Categories section

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,16 +11,16 @@ import { ListItem } from '@material-ui/core';
 export class CategoryItem extends Component {
   render() {
     return (
-      <div>
-        <ListItem classes={{root: "single-category-display-listItem"}}>
+
+
           <div className="single-category-display">
           
           <img className="category-image" src={this.props.category.image} alt={this.props.category.name} />
             {this.props.category.name}
           
           </div>
-        </ListItem>
-      </div>
+
+
     )
   }
 }
@@ -35,65 +35,6 @@ export default class Discover extends Component {
       {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
       {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
       {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-      {name: 'Books', image: 'https://thumbs.dreamstime.com/z/library-many-shelves-books-library-many-shelves-books-diminishing-perspective-shallow-dof-159717386.jpg'},
-      {name: 'Toys', image: 'https://thumbs.dreamstime.com/z/toys-collection-24515711.jpg'},
-      {name: 'Furniture', image: 'https://thumbs.dreamstime.com/z/natural-scandinavian-living-room-interior-wooden-furniture-mockup-picture-empty-white-wall-154024411.jpg'},
-      {name: 'Miscellaneous', image: 'https://thumbs.dreamstime.com/z/flea-market-brugge-belgium-bronze-baby-shoes-70263835.jpg'},
-      {name: 'Electronics', image: 'https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg'},
-
       
     ]
 
@@ -102,21 +43,103 @@ export default class Discover extends Component {
     return (
       <div>
         <div className="discover">
-        <div className="title-categories">Categories:</div>
-        <Grid container spacing={1}>
+        <h2 className="title-component-discover">Popular Categories:</h2>
+        <Grid container spacing={6}>
           <Grid 
             item xs={12}
-            classes={{root: "modified-grid-for-categories"}}
+
           >      
-            <Paper elevation={5} classes={{root: "modified-paper-for-categories"}}>
-              <List classes={{root: "modified-list-for-categories"}}>
-                {categoryItems}
-              </List>
+            <Paper elevation={5} >
+              <div className="display-all-items-wrapper">
+              {categoryItems}
+              </div>
             </Paper>
           </Grid>
         </Grid>
+
+        <Grid container spacing={6}>
+        <Grid 
+        item xs={4}
+        
+        >      
+        <h2 className="title-component-discover">Popular</h2>
+            <Paper elevation={5} >
+            <List>
+            <ListItem>
+            <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+            Cool PCB for LED Controls
+            </ListItem>
+            <ListItem>
+            <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+            Cool PCB for LED Controls
+            </ListItem>
+            <ListItem>
+            <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+            Cool PCB for LED Controls
+            </ListItem>
+            
+            </List>
+            </Paper>
+          </Grid>
+
+          <Grid 
+          item xs={4}
+
+        >      
+        <h2 className="title-component-discover">Ending Soon</h2>
+          <Paper elevation={5} >
+          <List>
+          <ListItem>
+          <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+          Cool PCB for LED Controls
+          </ListItem>
+          <ListItem>
+          <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+          Cool PCB for LED Controls
+          </ListItem>
+          <ListItem>
+          <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+          Cool PCB for LED Controls
+          </ListItem>
+          
+          </List>
+          </Paper>
+        </Grid>
+
+        <Grid 
+        item xs={4}
+
+      >      
+      <h2 className="title-component-discover">Suggested for you</h2>
+        <Paper elevation={5} >
+        <List>
+        <ListItem>
+        <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+        Cool PCB for LED Controls
+        </ListItem>
+        <ListItem>
+        <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+        Cool PCB for LED Controls
+        </ListItem>
+        <ListItem>
+        <img className="item-image" src="https://thumbs.dreamstime.com/z/cool-sharpen-electronics-15263.jpg" alt="..." />
+        Cool PCB for LED Controls
+        </ListItem>
+        
+        </List>
+        </Paper>
+      </Grid>
+        </Grid>
+
+
         </div>
       </div>
     )
   }
 }
+
+
+
+// classes={{root: "modified-grid-for-categories"}}
+// classes={{root: "modified-paper-for-categories"}}
+// classes={{root: "modified-list-for-categories"}}
