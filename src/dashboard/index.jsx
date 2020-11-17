fc57bc4debc1d5ed3db31e625e6090de28de12dd
Wilson Chen <wilson.chen@codeimmersives.com>
2020-11-17 14:17:01 -0500

    index

---
+++ src/dashboard/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './dashboard.scss';
+// import SideNav from './SideNav'
+// import TopNav from './TopNav'
+import Dashboard from './Dashboard';
+import Discover from '../pages/Discover';
+import Categories from '../pages/Categories';
+import Detail from '../pages/Detail';
+import MyListing from '../pages/MyListing';
+import AcceptDecline from '../pages/AcceptDecline';
+export default class index extends Component {
+  render() {
+    return (
+      <Dashboard>
+        {/*<Discover /> */}
+        {/*<Detail /> */}
+        {/* <MyListing /> */}
+        {/* <Categories /> */}
+        <AcceptDecline />
+      </Dashboard>
+    );
+  }
+}
